24b0048c14b788fa0b7d28ee862fb16d810399ba
Shammy Wammy <svassell2@brooklynsteamcenter.org>
2023-05-30 14:56:40 -0400

    gg

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
 </script>
 
 <template>
-
+<nav>
+    <h1>This is Harmonize</h1>
+    <router-link to="/About">About</router-link>
+    <router-link to="/Playlist"></router-link>
+    <RouterLink to="/"></RouterLink>
+</nav>
+<router-view />
 </template>
 
-<style scoped>
+<style>
 </style>
